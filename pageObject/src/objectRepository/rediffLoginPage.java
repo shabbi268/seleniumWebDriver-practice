7142316fe99d2f71388b1dabc18974cb2834b667
Kesa Shabarish <shabarish.shabbi@gmail.com>
2021-02-21 16:43:05 -0500

    used PageObjectFactory

--- pageObject/src/objectRepository/rediffLoginPage.java
+++ pageObject/src/objectRepository/rediffLoginPage.java
@@ -3,6 +3,8 @@ package objectRepository;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class rediffLoginPage {
 	
@@ -10,15 +12,20 @@ public class rediffLoginPage {
 	WebDriver driver;
 	public rediffLoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
-	By username = By.xpath(".//*[@id='login1']");
+//	By username = By.xpath(".//*[@id='login1']");
+	
+	@FindBy(xpath=".//*[@id='login1']")
+	WebElement username;
+	
 	By password = By.name("passwd");
 	By goButton = By.name("proceed");
 	By home = By.linkText("rediff.com");
 	
 	public WebElement EmailId() {
-		return driver.findElement(username);
+		return username;
 	}
 	
 	public WebElement Password() {
